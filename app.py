9d75f83c2a27b255c91a3cfd72ab4250ac03533a
Daniel Chavez <dachav26@g.holycross.edu>
2024-09-13 11:48:53 -0400

    Integrated Stock API, need to add css/html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, jsonify
+
 import yfinance as yf
 import datetime
 
@@ -12,17 +13,40 @@ def welcome():
 def dashboard():
     return render_template('FinanceAppUI.html')
 
-@app.route('/search', methods=['GET', 'POST'])
+# API Route for pulling the stock quote
+@app.route("/quote")
+def display_quote():
+	# get a stock ticker symbol from the query string
+	# default to AAPL
+	symbol = request.args.get('symbol', default="AAPL")
+
+	# pull the stock quote
+	quote = yf.Ticker(symbol)
+
+	#return the object via the HTTP Response
+	return jsonify(quote.info)
+
+# API route for pulling the stock history
+@app.route("/history")
+def display_history():
+	#get the query string parameters
+	symbol = request.args.get('symbol', default="AAPL")
+	period = request.args.get('period', default="1y")
+	interval = request.args.get('interval', default="1mo")
+
+	#pull the quote
+	quote = yf.Ticker(symbol)	
+	#use the quote to pull the historical data from Yahoo finance
+	hist = quote.history(period=period, interval=interval)
+	#convert the historical data to JSON
+	data = hist.to_json()
+	#return the JSON in the HTTP response
+	return data
+
+@app.route('/search')
 def search():
-    stock_symbol = request.args.get('stockSymbol', 'AAPL')  # Default to AAPL if no symbol provided
-    stock = yf.Ticker(stock_symbol)
-    today = datetime.datetime.today().strftime('%Y-%m-%d')
-    data = stock.history(start=today, end=today)
-    
-    # Convert data to a dictionary for easy use in the template
-    data_dict = data.reset_index().to_dict(orient='records')
-    
-    return render_template('search.html', data=data_dict, symbol=stock_symbol)
+    # we will use Flask's render_template method to render a website template.
+    return render_template("search.html")
 
 @app.route('/settings')
 def settings():
