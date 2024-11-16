cf322f3a9aea7722f09eeaaa1fb2697cacf6eaba
Dannychz7 <106276207+Dannychz7@users.noreply.github.com>
2024-11-16 15:09:06 -0500

    Added Login and Sign-Up Styling

--- ApexFinance-Django/ApexFinance/users/forms.py
+++ ApexFinance-Django/ApexFinance/users/forms.py
@@ -2,12 +2,22 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    password_confirm = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password', 'class': 'form-input'}))
+    password_confirm = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm your password', 'class': 'form-input'}))
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'password_confirm']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Enter your username', 'class': 'form-input'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Enter your email address', 'class': 'form-input'}),
+        }
+        labels = {
+            'username': '',
+            'email': '',
+            'password': '',
+            'password_confirm': ''
+        }
 
     def clean(self):
         cleaned_data = super().clean()
